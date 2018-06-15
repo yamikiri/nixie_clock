4227102d91c39a72c12119b4824f1ceb5f0681f6
Tylor Yang <tylor@solidyear.com.tw>
2018-06-15 16:47:10 +0800

    Disable one-time alarm after used

--- src/alarm.c
+++ src/alarm.c
@@ -5,6 +5,7 @@
 /* device.h includes */
 #include "mt7687.h"
 #include "system_mt7687.h"
+#include "nvdm.h"
 
 #include "sys_init.h"
 #include "task_def.h"
@@ -22,6 +23,7 @@ void checkAlarm(hal_rtc_time_t* t)
     uint32_t i = 0, j = 0;
     alarm_config *pac = NULL;
     static uint8_t preAlarmFired = 0;
+    nvdm_status_t nvdmStatus;
     // static uint8_t current
     for (i = 0; i < gConfig.nAlarms; i++) {
         pac = &(gConfig.alarms[i]);
@@ -37,6 +39,7 @@ void checkAlarm(hal_rtc_time_t* t)
 
                     if (pac->AlarmType == AlarmTypeOneshot) {
                         pac->AlarmEnable = 0;
+                        nvdmStatus = nvdm_write_data_item(NVDM_GRP, NVDM_GLOBAL_CONFIG, NVDM_DATA_ITEM_TYPE_RAW_DATA, (uint8_t *)&gConfig, sizeof(gConfig));
                     }
 
                     gAlarmMode = 1;
